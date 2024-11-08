b4d5350982503224b55e9260008303af3929c548
SANKALP KUMAR SAHOO <145092873+sankalpsahoo07@users.noreply.github.com>
2024-11-08 23:29:43 +0530

    Delete Smart_Contact_Manager directory

--- Smart_Contact_Manager/src/main/java/com/infy/smartcontact/repository/ContactRepository.java
+++
@@ -1,7 +0,0 @@
-package com.infy.smartcontact.repository;
-
-import com.infy.smartcontact.model.Contact;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactRepository extends JpaRepository<Contact,Long> {
-}
